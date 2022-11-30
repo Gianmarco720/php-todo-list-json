e385c16746832bb07783b95434b6b331d4483114
Gianmarco720 <orabonagianmarco379@gmail.com>
2022-11-30 16:12:52 +0100

    add comments

--- script.php
+++ script.php
@@ -1,6 +1,9 @@
 <?php
 
+// prendo il file json e la trasformo in stringa
 $tasks_string = file_get_contents('tasks.json');
+
+// trasformo il file json in un array di PHP
 $tasks_array = json_decode($tasks_string);
 
 header('Content-Type: application/json');
