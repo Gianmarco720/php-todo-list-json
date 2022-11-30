ed7ee4bc08c63d0462b3b1224a231a6072a0250c
Gianmarco720 <orabonagianmarco379@gmail.com>
2022-11-30 16:46:44 +0100

    push for ticket

--- script.php
+++ script.php
@@ -6,5 +6,22 @@ $tasks_string = file_get_contents('tasks.json');
 // trasformo il file json in un array di PHP
 $tasks_array = json_decode($tasks_string);
 
+
+if (isset($_POST['task'])) {
+    $task = $_POST['task'];
+
+    // pusho la nuova task nell'array
+    array_push($tasks_array, $task);
+
+    // trasformo l'array PHP in formato json
+    $json_tasks = json_encode($tasks_array);
+
+    // con file_put_contents lo scrivo all'interno del file json
+    file_put_contents('tasks.json', $json_tasks);
+
+    // trasformo di nuovo il file json in un array leggibile da PHP
+    $json_tasks = json_decode($tasks_string);
+}
+
 header('Content-Type: application/json');
 echo json_encode($tasks_array);
